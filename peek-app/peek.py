36e09f5a10fec2bfc42f2f145f910cdd555ce0a5
John Sattari <johnsattari@johns-air.lan>
2022-08-30 21:45:04 -0700

    updated peek.py with new list function

--- peek-app/peek.py
+++ peek-app/peek.py
@@ -5,15 +5,31 @@ import pathlib
 
 
 def data_filter(data: list, field: str, filter_value: str) -> list:
-    """Filter data by choice of field and desired value
+    """
+    Filter data by choice of field and desired value
 
-    :param data: list - dataset that will be filtered
-    :param field: str - field that will be used as basis for filtration
-    :param filter_value: str - value that will be used as the filter
-    :return: list - a list of dicts filtered by the desired value
+    Values:
+        data (list):            dataset that will be filtered
+        field (str):            field that will be used as basis for filtration
+        filter_value (str):     value that will be used as the filter
+
+    Return (list):              a list of dicts filtered by the desired value
     """
 
     return [dictionary for dictionary in data if dictionary[field] == filter_value]
+
+
+def list_of_units(data: list) -> list:
+    """
+    Provides a list of available fields within a csv
+
+    Values:
+        data (list):    dataset that will contain list of fields
+
+    Return (list):      a list of fields from dataset
+    """
+
+    return [key for key in data[0].keys()]
 
 
 file_path = pathlib.Path("peek-app")
@@ -24,4 +40,4 @@ with open(file_to_open, "r") as file:
     reader = csv.DictReader(file)
     data_dict = [{key: value for key, value in row.items()} for row in reader]
 
-print(data_filter(data_dict, "winner", "Italy"))
+print(list_of_units(data_dict))
