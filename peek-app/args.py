e0ff76f2004ae7ae6fb581fe223e225b960efcb3
John Sattari <johnsattari@johns-air.lan>
2022-09-07 15:04:48 -0700

    removed subparsers; reverted to multi arg structure

--- peek-app/args.py
+++ peek-app/args.py
@@ -5,10 +5,8 @@ def get_args():
     """
     Parse console args and return three variables
     that contain commands, flags, and additional args
-
     Values:
         None:           Accepts inputs and returns values
-
     Return (list):      Three variables that contains the path to dataset,
                         flags, fields (if any)
     """
@@ -18,44 +16,23 @@ def get_args():
         description="A Python CLI that helps you preview your datasets"
     )
 
-    # subparser object
-    subparser = parser.add_subparsers(
-        title="commands",
-        dest="command"
-    )
-
-    # subparser and arg for filepath
-    file_parser = subparser.add_parser(
-        name="filepath",
-        description="filepath of dataset",
-        help="filepath of dataset"
-    )
-    file_parser.add_argument(
+    # arg and arg for filepath
+    parser.add_argument(
         "-f", "--filepath",
         required=True,
         dest="filepath",
         help="filepath of dataset")
 
-    # subparser and arg for search
-    search_parser = subparser.add_parser(
-        name="search",
-        description="Search for matching results based on field and value",
-        help="Search for matching results based on field and value"
-    )
-    search_parser.add_argument(
+    # arg and arg for search
+    parser.add_argument(
         "-s", "--search",
         nargs=2,
         dest="search",
         default=False,
         help="Search for matching results based on field and value")
 
-    # subparser and arg for listing fields
-    field_parser = subparser.add_parser(
-        name="list",
-        description="Returns list of fields available for filtration",
-        help="Returns list of fields available for filtration"
-    )
-    field_parser.add_argument(
+    # arg for listing fields
+    parser.add_argument(
         "-l", "--list",
         dest="list",
         action="store_true",
@@ -63,7 +40,7 @@ def get_args():
         help="Returns list of fields available for filtration"
     )
 
-    # func for head of data
+    # arg for head of data
     parser.add_argument(
         "-hd", "--head",
         dest="head",
@@ -72,7 +49,7 @@ def get_args():
         help="Shows preview of first 5 rows of dataset"
     )
 
-    # func for tail of data
+    # arg for tail of data
     parser.add_argument(
         "-t", "--tail",
         dest="tail",
@@ -88,5 +65,6 @@ def get_args():
         vars(args).items())[1:] if tup[1] is not False][0]
     flags = commands[0]
     fields = commands[1]
+    print(data, commands, flags, fields, sep="\n")
 
     return data, commands, flags, fields
